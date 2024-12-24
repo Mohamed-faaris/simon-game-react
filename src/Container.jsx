87df6a755a47f69e8364ab527400c411812aa3a6
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2024-12-24 16:53:16 +0530

    simple working frame

--- src/Container.jsx
+++ src/Container.jsx
@@ -7,9 +7,11 @@ function random(max) {
 }
 
 function Container(props, currentIndex = index) {
-  let colors = [1,2,3,4]
+  let colors = []
   let round = 0
+  let score = 0
   let refs = [useRef(null),useRef(null),useRef(null),useRef(null)]
+
 
   function addColor(){
     colors.push(random(4));
@@ -17,12 +19,12 @@ function Container(props, currentIndex = index) {
   }
 
   async function playSequence(currentIndex){
-    //refs[colors[currentIndex]].current.classList.add("Glow");
+    refs[colors[currentIndex]].current.classList.add("Glow");
     console.log(colors[currentIndex],"on");
     await delay(50);
     console.log(colors[currentIndex],"off");
-    //refs[colors[currentIndex]].current.classList.remove("Glow");
-    if(currentIndex < round)
+    refs[colors[currentIndex]].current.classList.remove("Glow");
+    if(currentIndex < score)
       await playSequence(currentIndex + 1);
   }
 
@@ -31,7 +33,10 @@ function Container(props, currentIndex = index) {
   // }, [colors]);
 
   function resetGame() {
-   round = 0
+    round = 0
+    colors =[]
+    score = 0
+    addColor()
   }
 
   function check(i){
@@ -39,6 +44,8 @@ function Container(props, currentIndex = index) {
       round +=1 ;
       if(round === colors.length){
         addColor();
+        score += 1;
+        playSequence(0)
         round = 0;
       }
       return true;
@@ -48,12 +55,12 @@ function Container(props, currentIndex = index) {
   }
 
   return (
-          <div className="Container">
+          <div className="Container" >
             <BoxButton  color="blue" onClickEvent ={check} ref ={refs[0]} index={1}/>
             <BoxButton  color="green" onClickEvent ={check} ref ={refs[1]} index={2}/>
             <BoxButton  color="red" onClickEvent ={check} ref ={refs[2]} index={3}/>
             <BoxButton  color="yellow" onClickEvent ={check} ref ={refs[3]} index={4}/>
-            {/*<button onClick={addColor}>add</button>*/}
+            <button onClick={()=>(addColor())}>add</button>
         </div>
     );
 }
