e8afb244bcc91aa2f0ef259cbcecdf70844bf96c
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2024-12-24 16:29:25 +0530

    simple working frame

--- src/BoxButton.jsx
+++ src/BoxButton.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 
-function BoxButton(props) {
-    return (
+const BoxButton = forwardRef((props,ref) => (
         <div
-            className="BoxButton"
-            style={{ backgroundColor: props.color || "black" }}
-            onClick={()=>console.log(props.onClickEvent(props.index),props.index)}
+                className={`BoxButton`}
+                style={{backgroundColor: props.color || "black"}}
+                ref={props.ref}
+                onClick={() => {
+                  console.log(props.onClickEvent(props.index), props.index);
+                }}
         >
         </div>
-    );
-}
+));
 
 export default BoxButton;
--- src/Container.jsx
+++ src/Container.jsx
@@ -1,18 +1,39 @@
 import React, { useEffect, useState, useRef } from 'react';
 import BoxButton from "./BoxButton.jsx";
+import {delay} from "./utils.jsx";
 
 function random(max) {
     return Math.floor(Math.random() * max)+1;
 }
 
-function Container(props) {
-  let colors = [1]
+function Container(props, currentIndex = index) {
+  let colors = [1,2,3,4]
   let round = 0
+  let refs = [useRef(null),useRef(null),useRef(null),useRef(null)]
 
   function addColor(){
     colors.push(random(4));
     console.log(colors);
   }
+
+  async function playSequence(currentIndex){
+    //refs[colors[currentIndex]].current.classList.add("Glow");
+    console.log(colors[currentIndex],"on");
+    await delay(50);
+    console.log(colors[currentIndex],"off");
+    //refs[colors[currentIndex]].current.classList.remove("Glow");
+    if(currentIndex < round)
+      await playSequence(currentIndex + 1);
+  }
+
+  // useEffect(async () => {
+  //   await playSequence(0)
+  // }, [colors]);
+
+  function resetGame() {
+   round = 0
+  }
+
   function check(i){
     if(i === colors[round]){
       round +=1 ;
@@ -22,16 +43,16 @@ function Container(props) {
       }
       return true;
     }
-    round = 0;//place reset with reset game
+    resetGame();
     return false;
   }
 
   return (
           <div className="Container">
-            <BoxButton  color="blue" onClickEvent ={check} index={1}/>
-            <BoxButton  color="green" onClickEvent ={check} index={2}/>
-            <BoxButton  color="red" onClickEvent ={check} index={3}/>
-            <BoxButton  color="yellow" onClickEvent ={check} index={4}/>
+            <BoxButton  color="blue" onClickEvent ={check} ref ={refs[0]} index={1}/>
+            <BoxButton  color="green" onClickEvent ={check} ref ={refs[1]} index={2}/>
+            <BoxButton  color="red" onClickEvent ={check} ref ={refs[2]} index={3}/>
+            <BoxButton  color="yellow" onClickEvent ={check} ref ={refs[3]} index={4}/>
             {/*<button onClick={addColor}>add</button>*/}
         </div>
     );
