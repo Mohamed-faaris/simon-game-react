1fdca669e61f98427e969a9abedcadde84c4429d
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2025-01-02 01:44:38 +0530

    fixed sounds

--- src/utils.jsx
+++ src/utils.jsx
@@ -2,8 +2,8 @@ export function delay(ms){
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export const blueClick = new Audio("../sounds/blue.mp3");
-export const redClick = new Audio("../sounds/red.mp3");
-export const greenClick = new Audio("../sounds/green.mp3");
-export const yellowClick = new Audio("../sounds/yellow.mp3");
-export const wrongClick = new Audio("../sounds/wrong.mp3");
+export const blueClick = new Audio("./sounds/blue.mp3");
+export const redClick = new Audio("./sounds/red.mp3");
+export const greenClick = new Audio("./sounds/green.mp3");
+export const yellowClick = new Audio("./sounds/yellow.mp3");
+export const wrongClick = new Audio("./sounds/wrong.mp3");
