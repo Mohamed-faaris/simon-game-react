2f5874070a6bbbf1979ab468409d6545c10b0e68
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2025-01-04 03:10:00 +0530

    improve styling

--- src/SimonGame.jsx
+++ src/SimonGame.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useRef } from 'react';
 import BoxButton from "./BoxButton.jsx";
 import {blueClick, delay, greenClick, redClick, yellowClick} from "./utils.jsx";
+import { Play } from 'lucide-react';
 
 function random(max) {
     return Math.floor(Math.random() * max);
@@ -69,17 +70,20 @@ function SimonGame(props, currentIndex = index) {
   }
 
   return (
-					<div className={"SimonGame"}>
+					<div className={"SimonGame "}>
 						<p>{colors.current.length===0?"click play button to start":`score ${score.current}`}</p>
 	          <div className="Container" >
 	            <BoxButton  color="blue" onClickEvent ={check}  index={0} style={style[0]}/>
 	            <BoxButton  color="green" onClickEvent ={check}  index={1} style={style[1]}/>
 	            <BoxButton  color="red" onClickEvent ={check}  index={2} style={style[2]}/>
 	            <BoxButton  color="yellow" onClickEvent ={check}  index={3} style={style[3]}/>
-	            <button onClick={()=>{
+	            <button
+					            className=" flex justify-center items-center bg-gray-700 text-white text-2xl" onClick={()=>{
 											if(colors.current.length===0)addColor();
 					            playSequence(0);
-	            }}>{colors.current.length===0?">":`${score.current}`}</button>
+	            }}>
+		            {colors.current.length === 0 ? <Play /> : `${score.current}`}
+							</button>
 	          </div>
 					</div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import {StrictMode, useRef} from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import BoxButton from "./BoxButton.jsx";
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
